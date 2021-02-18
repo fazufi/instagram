77d2e405e1b82c0de4761a6a8d03a38092c6d115
fazufi <fazufi10@gmail.com>
2021-02-18 13:29:56 +0700

    Line baru

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// line baru
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
